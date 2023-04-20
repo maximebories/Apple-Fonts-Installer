6efd10fb6ad53c842ddad3fcf51359b62ba07411
Maxime Bories <30550722+maximebories@users.noreply.github.com>
2023-04-20 03:02:15 +0200

    Unmount the volumes and remove the images

--- setup.sh
+++ setup.sh
@@ -17,17 +17,27 @@ fontVolumes=("SFProFonts" "SFCompactFonts" "SFMonoFonts" "NYFonts")
 # Set the font package names
 fontPackages=("SF Pro Fonts.pkg" "SF Compact Fonts.pkg" "SF Mono Fonts.pkg"  "NY Fonts.pkg")
 
-# Download the fonts
+# Download the font images
 for image in "${fontImages[@]}"; do
 	curl -O "${baseURL}${image}"
 done
 
-# Mount the DMG files
+# Mount the font images
 for image in "${fontImages[@]}"; do
 	hdiutil attach "${image}"
 done
 
-# Silently run the installer for each font
+# Silently run the installer for each font package
 for i in "${!fontVolumes[@]}"; do
 	sudo installer -pkg "/Volumes/${fontVolumes[$i]}/${fontPackages[$i]}" -target /
+done
+
+# Unmount the font volumes
+for volume in "${fontVolumes[@]}"; do
+	hdiutil detach "/Volumes/${volume}"
+done
+
+# Remove the font images
+for image in "${fontImages[@]}"; do
+	rm "${image}"
 done
